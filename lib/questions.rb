21c864e5b2ccfdb5937ac8337a242cc110669f42
IAN SIMPSON <ianconradsimpson@gmail.com>
2017-06-25 15:07:15 +0100

    33 finished 6 more to go

--- lib/questions.rb
+++ lib/questions.rb
@@ -61,6 +61,11 @@ end
 # turn a positive integer into a negative integer. A negative integer
 # stays negative
 def make_numbers_negative(number)
+  if number > 0
+    number * -1
+  else
+    number
+  end
 end
 
 # turn an array of numbers into two arrays of numbers, one an array of
@@ -68,6 +73,10 @@ end
 # even numbers come first
 # so [1, 2, 3, 4, 5, 6] becomes [[2, 4, 6], [1, 3, 5]]
 def separate_array_into_even_and_odd_numbers(array)
+  even_array = []
+  odd_array = []
+  array.each { |n| n.even? ? even_array << n : odd_array << n}
+  [even_array, odd_array]
 end
 
 # count the numbers of elements in an element which are palindromes
@@ -75,33 +84,45 @@ end
 # e.g. 'bob'. So in the array ['bob', 'radar', 'eat'], there
 # are 2 palindromes (bob and radar), so the method should return 2
 def number_of_elements_that_are_palindromes(array)
+  palindromes = []
+  array.each{ |w| w == w.reverse ? palindromes << w : false}
+  palindromes.length
 end
 
 # return the shortest word in an array
 def shortest_word_in_array(array)
+  array.sort! { |a, b| a.length <=> b.length }
+  array[0]
 end
 
 # return the shortest word in an array
 def longest_word_in_array(array)
+  array.sort! { |a, b| a.length <=> b.length }
+  array[-1]
 end
 
 # add up all the numbers in an array, so [1, 3, 5, 6]
 # returns 15
 def total_of_array(array)
+  array.inject(:+)
 end
 
 # turn an array into itself repeated twice. So [1, 2, 3]
 # becomes [1, 2, 3, 1, 2, 3]
 def double_array(array)
+  array * 2
 end
 
 # convert a symbol into a string
 def turn_symbol_into_string(symbol)
+  symbol.to_s
 end
 
 # get the average from an array, rounded to the nearest integer
 # so [10, 15, 25] should return 17
 def average_of_array(array)
+  sum_of_array = (array.inject(:+)).to_f
+  (sum_of_array / array.length).ceil
 end
 
 # get all the elements in an array, up until the first element
@@ -109,12 +130,14 @@ end
 # [1, 3, 5, 4, 1, 2, 6, 2, 1, 3, 7]
 # becomes [1, 3, 5, 4, 1, 2]
 def get_elements_until_greater_than_five(array)
+  array.take_while{ |n| n <= 5}
 end
 
 # turn an array (with an even number of elements) into a hash, by
 # pairing up elements. e.g. ['a', 'b', 'c', 'd'] becomes
 # {'a' => 'b', 'c' => 'd'}
 def convert_array_to_a_hash(array)
+  new_hash = Hash[*array]
 end
 
 # get all the letters used in an array of words and return
@@ -122,43 +145,52 @@ end
 # . e.g. the array ['cat', 'dog', 'fish'] becomes
 # ['a', 'c', 'd', 'f', 'g', 'h', 'i', 'o', 's', 't']
 def get_all_letters_in_array_of_words(array)
+  array.join().split('').sort
 end
 
 # swap the keys and values in a hash. e.g.
 # {'a' => 'b', 'c' => 'd'} becomes
 # {'b' => 'a', 'd' => 'c'}
 def swap_keys_and_values_in_a_hash(hash)
+  hash.invert
 end
 
 # in a hash where the keys and values are all numbers
 # add all the keys and all the values together, e.g.
 # {1 => 1, 2 => 2} becomes 6
 def add_together_keys_and_values(hash)
+  array = hash.inject(:+)
+  array.inject(:+)
 end
 
 # take out all the capital letters from a string
 # so 'Hello JohnDoe' becomes 'ello ohnoe'
 def remove_capital_letters_from_string(string)
+  string.gsub(/[A-Z]/, '')
 end
 
 # round up a float up and convert it to an Integer,
 # so 3.214 becomes 4
 def round_up_number(float)
+  float.ceil
 end
 
 # round down a float up and convert it to an Integer,
 # so 9.52 becomes 9
 def round_down_number(float)
+  float.floor
 end
 
 # take a date and format it like dd/mm/yyyy, so Halloween 2013
 # becomes 31/10/2013
 def format_date_nicely(date)
+  date.strftime("%d/%m/%Y")
 end
 
 # get the domain name *without* the .com part, from an email address
 # so alex@makersacademy.com becomes makersacademy
 def get_domain_name_from_email_address(email)
+  email.split("@")[1].split(".com")[0]
 end
 
 # capitalize the first letter in each word of a string,
@@ -167,22 +199,28 @@ end
 # 'the lion the witch and the wardrobe' becomes
 # 'The Lion the Witch and the Wardrobe'
 def titleize_a_string(string)
+  str = string.capitalize.split
+  str.map!{|x| ['a', 'and', 'the'].include?(x) ? x : x.capitalize}.join(' ')
 end
 
 # return true if a string contains any special characters
 # where 'special character' means anything apart from the letters
 # a-z (uppercase and lower) or numbers
 def check_a_string_for_special_characters(string)
+  string.match(/\W/) ? true : false
+  # /\W/ - a non-word charcter. /\w/ would - a word character
 end
 
 # get the upper limit of a range. e.g. for the range 1..20, you
 # should return 20
 def get_upper_limit_of(range)
+  range.last
 end
 
 # should return true for a 3 dot range like 1...20, false for a
 # normal 2 dot range
 def is_a_3_dot_range?(range)
+  range.last != range.max ? true : false
 end
 
 # get the square root of a number
